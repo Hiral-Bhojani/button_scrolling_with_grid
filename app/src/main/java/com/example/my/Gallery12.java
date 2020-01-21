1768e007439c7c7c01f12d90aebdd41318c08f6d
HIRAL_BHOJANI <hiralg4017@gmail.com>
2020-01-22 02:02:50 +0530

    Commmit2 for scrolling button with grid in navigation drawer activity

---
+++ app/src/main/java/com/example/my/Gallery12.java
@@ -0,0 +1,14 @@
+package com.example.my;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gallery12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery12);
+    }
+}
